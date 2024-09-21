a580653aed22e46fed17e4d3d992ebc6d9e647b7
Mohit Pardhi <mpardhi11@gmail.com>
2024-09-21 19:33:42 +0530

    Add new pages and update routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import About from "./pages/About";
+import Appointments from "./pages/Appointments";
+import Contact from "./pages/Contact";
+import Doctors from "./pages/Doctors";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MyAppointments from "./pages/MyAppointments";
+import MyProfile from "./pages/MyProfile";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="mx-4 sm:mx-[10%] ">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/appointments/:doctorId" element={<Appointments />} />
+      </Routes>
+    </div>
+  );
 }
